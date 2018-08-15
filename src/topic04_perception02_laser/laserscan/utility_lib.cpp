1259abca16a48b0be8fea7a0338bb00a5d2e7296
Anis Koubaa <anis.koubaa@gmail.com>
2018-08-15 19:24:06 +0300

    fix scan libraries dependencies

--- src/topic04_perception02_laser/laserscan/utility_lib.cpp
+++ src/topic04_perception02_laser/laserscan/utility_lib.cpp
@@ -1,13 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
-#include <iostream>
-#include <iomanip>
 #include "utility_lib.h"
 
 using namespace std;
