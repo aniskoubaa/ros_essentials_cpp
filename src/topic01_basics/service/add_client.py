ce5dd992f4a0894976207331878c7504cfb05e20
Anis Koubaa <aniskoubaa@users.noreply.github.com>
2020-09-17 11:17:13 +0300

    fixing script for python 3

--- src/topic01_basics/service/add_client.py
+++ src/topic01_basics/service/add_client.py
@@ -12,19 +12,19 @@ def add_two_ints_client(x, y):
         add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
         resp1 = add_two_ints(x, y)
         return resp1.sum
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+    except rospy.ServiceException(e):
+        print ("Service call failed: %s"%e)
 
 def usage():
-    return "%s [x y]"%sys.argv[0]
+    return 
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
         x = int(sys.argv[1])
         y = int(sys.argv[2])
     else:
-        print usage()
+        print ("%s [x y]"%sys.argv[0])
         sys.exit(1)
-    print "Requesting %s+%s"%(x, y)
+    print ("Requesting %s+%s"%(x, y))
     s = add_two_ints_client(x, y)
-    print "%s + %s = %s"%(x, y, s)
+    print ("%s + %s = %s"%(x, y, s))
