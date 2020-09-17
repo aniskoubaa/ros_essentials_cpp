ff2c9b8deb8ee8ca9b4b2122d8c36fe0e648c921
Anis Koubaa <aniskoubaa@users.noreply.github.com>
2020-09-17 11:17:41 +0300

    fix script for python 3

--- src/topic01_basics/service/add_server.py
+++ src/topic01_basics/service/add_server.py
@@ -7,13 +7,13 @@ from ros_essentials_cpp.srv import AddTwoIntsResponse
 import rospy
 
 def handle_add_two_ints(req):
-    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+    print ("Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b)))
     return AddTwoIntsResponse(req.a + req.b)
 
 def add_two_ints_server():
     rospy.init_node('add_two_ints_server')
     s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
-    print "Ready to add two ints."
+    print ("Ready to add two ints.")
     rospy.spin()
     
 if __name__ == "__main__":
