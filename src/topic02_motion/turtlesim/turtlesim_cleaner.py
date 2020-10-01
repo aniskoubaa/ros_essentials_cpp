165c331c23facd19f0280ba8e83ad2dcbf37f255
Anis Koubaa <aniskoubaa@users.noreply.github.com>
2020-10-01 11:27:24 +0300

    fixing some errors in the python script

--- src/topic02_motion/turtlesim/turtlesim_cleaner.py
+++ src/topic02_motion/turtlesim/turtlesim_cleaner.py
@@ -7,10 +7,6 @@ import math
 import time
 from std_srvs.srv import Empty
 
-x=0
-y=0
-yaw=0
-
 def poseCallback(pose_message):
     global x
     global y, yaw
@@ -24,7 +20,7 @@ def poseCallback(pose_message):
     #print ('yaw = {}'.format(pose_message.theta)) #new in python 3
 
 
-def move(speed, distance, is_forward):
+def move(velocity_publisher, speed, distance, is_forward):
         #declare a Twist message to send velocity commands
         velocity_message = Twist()
         #get current location 
@@ -39,19 +35,16 @@ def move(speed, distance, is_forward):
 
         distance_moved = 0.0
         loop_rate = rospy.Rate(10) # we publish the velocity at 10 Hz (10 times a second)    
-        cmd_vel_topic='/turtle1/cmd_vel'
-        velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
-
+        
         while True :
                 rospy.loginfo("Turtlesim moves forwards")
                 velocity_publisher.publish(velocity_message)
 
                 loop_rate.sleep()
                 
-                #rospy.Duration(1.0)
-                
-                distance_moved = abs(0.5 * math.sqrt(((x-x0) ** 2) + ((y-y0) ** 2)))
-                print  distance_moved               
+                distance_moved = abs(math.sqrt(((x-x0) ** 2) + ((y-y0) ** 2)))
+                print  (distance_moved)
+                print(x)
                 if  not (distance_moved<distance):
                     rospy.loginfo("reached")
                     break
@@ -60,19 +53,10 @@ def move(speed, distance, is_forward):
         velocity_message.linear.x =0
         velocity_publisher.publish(velocity_message)
     
-def rotate (angular_speed_degree, relative_angle_degree, clockwise):
+def rotate (velocity_publisher, angular_speed_degree, relative_angle_degree, clockwise):
     
-    global yaw
     velocity_message = Twist()
-    velocity_message.linear.x=0
-    velocity_message.linear.y=0
-    velocity_message.linear.z=0
-    velocity_message.angular.x=0
-    velocity_message.angular.y=0
-    velocity_message.angular.z=0
 
-    #get current location 
-    theta0=yaw
     angular_speed=math.radians(abs(angular_speed_degree))
 
     if (clockwise):
@@ -106,19 +90,17 @@ def rotate (angular_speed_degree, relative_angle_degree, clockwise):
     velocity_publisher.publish(velocity_message)
 
 
-def go_to_goal(x_goal, y_goal):
+def go_to_goal(velocity_publisher, x_goal, y_goal):
     global x
     global y, yaw
 
     velocity_message = Twist()
-    cmd_vel_topic='/turtle1/cmd_vel'
 
     while (True):
         K_linear = 0.5 
         distance = abs(math.sqrt(((x_goal-x) ** 2) + ((y_goal-y) ** 2)))
 
         linear_speed = distance * K_linear
-
 
         K_angular = 4.0
         desired_angle_goal = math.atan2(y_goal-y, x_goal-x)
@@ -128,57 +110,51 @@ def go_to_goal(x_goal, y_goal):
         velocity_message.angular.z = angular_speed
 
         velocity_publisher.publish(velocity_message)
-        
-        #print velocity_message.linear.x
-        #print velocity_message.angular.z
-        print 'x=', x, 'y=',y
-
+        print ('x=', x, ', y=',y, ', distance to goal: ', distance)
 
         if (distance <0.01):
             break
 
-def setDesiredOrientation(desired_angle_radians):
-    relative_angle_radians = desired_angle_radians - yaw
+def setDesiredOrientation(publisher, speed_in_degree, desired_angle_degree):
+    relative_angle_radians = math.radians(desired_angle_degree) - yaw
+    clockwise=0
     if relative_angle_radians < 0:
         clockwise = 1
     else:
         clockwise = 0
-    print relative_angle_radians
-    print desired_angle_radians
-    rotate(30 ,math.degrees(abs(relative_angle_radians)), clockwise)
+    print ("relative_angle_radians: ",math.degrees(relative_angle_radians))
+    print ("desired_angle_degree: ",desired_angle_degree)
+    rotate(publisher, speed_in_degree,math.degrees(abs(relative_angle_radians)), clockwise)
 
-def gridClean():
+def gridClean(publisher):
  
     desired_pose = Pose()
     desired_pose.x = 1
     desired_pose.y = 1
     desired_pose.theta = 0
  
-    moveGoal(desired_pose, 0.01)
+    go_to_goal(publisher, 1,1)
  
-    setDesiredOrientation(degrees2radians(desired_pose.theta))
+    setDesiredOrientation(publisher, 30, math.radians(desired_pose.theta))
  
-    move(2.0, 9.0, True)
-    rotate(degrees2radians(20), degrees2radians(90), False)
-    move(2.0, 9.0, True)
-    rotate(degrees2radians(20), degrees2radians(90), False)
-    move(2.0, 1.0, True)
-    rotate(degrees2radians(20), degrees2radians(90), False)
-    move(2.0, 9.0, True)
-    rotate(degrees2radians(30), degrees2radians(90), True)
-    move(2.0, 1.0, True)
-    rotate(degrees2radians(30), degrees2radians(90), True)
-    move(2.0, 9.0, True)
+    for i in range(5):
+        move(publisher, 2.0, 1.0, True)
+        rotate(publisher, 20, 90, False)
+        move(publisher, 2.0, 9.0, True)
+        rotate(publisher, 20, 90, True)
+        move(publisher, 2.0, 1.0, True)
+        rotate(publisher, 20, 90, True)
+        move(publisher, 2.0, 9.0, True)
+        rotate(publisher, 20, 90, False)
     pass
+
  
  
-def spiralClean():
+def spiralClean(velocity_publisher, wk, rk):
     vel_msg = Twist()
     loop_rate = rospy.Rate(1)
-    wk = 4
-    rk = 0
  
-    while((currentTurtlesimPose.x<10.5) and (currentTurtlesimPose.y<10.5)):
+    while((x<10.5) and (y<10.5)):
         rk=rk+1
         vel_msg.linear.x =rk
         vel_msg.linear.y =0
@@ -208,10 +184,11 @@ if __name__ == '__main__':
         pose_subscriber = rospy.Subscriber(position_topic, Pose, poseCallback) 
         time.sleep(2)
 
-        #move(1.0, 2.0, False)
-        #rotate(30, 90, True)
-        go_to_goal(1.0, 1.0)
-        #setDesiredOrientation(math.radians(90))
-       
+        #move(velocity_publisher, 1.0, 9.0, True)
+        #rotate(velocity_publisher, 30, 90, True)
+        #go_to_goal(velocity_publisher, 2.0, 1.5)
+        #setDesiredOrientation(velocity_publisher, 30, 90)
+        #spiralClean(velocity_publisher, 4, 0)
+        gridClean(velocity_publisher)
     except rospy.ROSInterruptException:
         rospy.loginfo("node terminated.")
