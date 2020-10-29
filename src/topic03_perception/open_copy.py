f2c7fdf640c78cd9b0e37b296827e2371208846d
Anis Koubaa <aniskoubaa@users.noreply.github.com>
2020-10-29 14:03:03 +0300

    Update open_copy.py

--- src/topic03_perception/open_copy.py
+++ src/topic03_perception/open_copy.py
@@ -9,17 +9,17 @@ import cv2
 
 image_name = "flower"
 
-print 'read an image from file'
+print ('read an image from file')
 img = cv2.imread("images/"+image_name+".jpg")
 
-print 'create a window holder for the image'
+print ('create a window holder for the image')
 cv2.namedWindow("Image",cv2.WINDOW_NORMAL)
 
-print 'display the image'
+print ('display the image')
 cv2.imshow("Image",img)
 
-print 'press a key inside the image to make a copy'
+print ('press a key inside the image to make a copy')
 cv2.waitKey(0)
 
-print 'image copied to folder images/copy/'
+print ('image copied to folder images/copy/')
 cv2.imwrite("images/copy/"+image_name+"-copy.jpg",img)
